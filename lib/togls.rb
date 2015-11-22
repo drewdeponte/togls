dad24faea09632348d0694c75e31b990f45ca434
Andrew De Ponte <cyphactor@gmail.com>
2015-11-21 17:51:48 -0800

    Rename feature_toggles to block
    **Why you made the change**
    
    I did this because I think it communicates the argument better in my
    opinion.

--- lib/togls.rb
+++ lib/togls.rb
@@ -21,9 +21,9 @@ require "togls/rules"
 require "logger"
 
 module Togls
-  def self.features(&feature_toggles)
-    if !feature_toggles.nil?
-      @feature_toggle_registry = FeatureToggleRegistry.create(&feature_toggles)
+  def self.features(&block)
+    if !block.nil?
+      @feature_toggle_registry = FeatureToggleRegistry.create(&block)
     else
       if @feature_toggle_registry.nil?
         raise Togls::NoFeaturesError, "Need to define features before you can get them"
