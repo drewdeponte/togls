6264dee01b8a4b4106aa108b211e9b1041e3b0ce
Ryan Hedges <ryanhedges@gmail.com>
2016-06-02 18:33:18 -0700

    Fix failing tests that need a valid target type when created
    I did this because we need to have a valid target type.

--- spec/togls/toggle_spec.rb
+++ spec/togls/toggle_spec.rb
@@ -84,10 +84,12 @@ describe Togls::Toggle do
 
           rule_klass = Class.new(Togls::Rule) do
             def self.target_type
-              Togls::TargetTypes::NOT_SET
+              :hoopty
             end
           end
           rule = rule_klass.new
+          allow(rule_klass).to receive(:target_type).and_return(Togls::TargetTypes::NOT_SET)
+
           result = toggle.target_matches?(rule)
           expect(result).to eql false
         end
@@ -186,10 +188,11 @@ describe Togls::Toggle do
 
           rule_klass = Class.new(Togls::Rule) do
             def self.target_type
-              Togls::TargetTypes::NOT_SET
+              :foo
             end
           end
           rule = rule_klass.new
+          allow(rule_klass).to receive(:target_type).and_return(Togls::TargetTypes::NOT_SET)
 
           expect(Togls.logger).to receive(:warn).with("Rule (id: #{rule.id}) cannot have target type of :not_set")
           toggle.target_matches?(rule)
@@ -256,10 +259,11 @@ describe Togls::Toggle do
 
           rule_klass = Class.new(Togls::Rule) do
             def self.target_type
-              Togls::TargetTypes::NOT_SET
+              :foo
             end
           end
           rule = rule_klass.new
+          allow(rule_klass).to receive(:target_type).and_return(Togls::TargetTypes::NOT_SET)
 
           expect(Togls.logger).to receive(:warn).with("Rule (id: #{rule.id}) cannot have target type of :not_set")
           toggle.target_matches?(rule)
